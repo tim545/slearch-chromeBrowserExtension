f143cecc33a44345ed32c2e6e73bfeb23b9ebaf3
Tim Freedman <tim.freedman@live.com.au>
2015-05-05 12:12:14 +1000

    abstract adding search bars to list, abstract initialization, fix event listener

--- slearch.js
+++ slearch.js
@@ -15,7 +15,7 @@ var slearch = {
 		// Add search bars to list
 		for (var i = 0; i < Html5SearchBars.length; i++) {
 			var sb = Html5SearchBars[i];
-			this.bars.push(sb);
+			this.addSearchBar(sb);
 		}
 	},
 
@@ -33,39 +33,54 @@ var slearch = {
 			sbAtttr.push(sb.id);
 			sbAtttr.push(sb.placeholder);
 			if (sb.getAttribute("aria-label") != null) sbAtttr.push(sb.getAttribute("aria-label"));
-			// sbAtttr.push(sb.ariaLabel);
 			for (var i = 0; i < sbAtttr.length; i++) {
 				var attribute = sbAtttr[i];
 				if (attribute.match(matchMaker)) {
-					this.bars.push(sb);
+					this.addSearchBar(sb);
 				}
 			}
 		}
 	},
 
+	// Adds a newly found search bar to the object store
+	addSearchBar: function(searchBar) {
+		var validSearchBar = true;
+		// Find if the search bar already exists
+		for (var i = 0; i < this.bars.length; i++) {
+			var bar = this.bars[i]
+			if (bar === searchBar) { validSearchBar = false; }
+		}
+		// Add the search bar
+		if (validSearchBar) {
+			this.bars.push(searchBar);
+		}
+		// For debug. TODO: delete
+		// if (slearch.bars.length > 0) {
+		// 	console.log("Slearch says: there are "+ slearch.bars.length +" search bars on this page:", slearch.bars);
+		// } else {
+		// 	console.log("no search bars found");
+		// }
+	},
+
 	// When a user pressed the '/' key we want to highlight a search bar
 	mapActions: function() {
 		window.onkeypress = function(e) {
-			console.log("keypress");
-			if (e.keyCode === "0xBF (191)" || e.charCode === "0xBF (191)" || e.key === "AKEYCODE_SLASH" || e.key === "VK_DIVIDE") {
+			if (e.keyCode === 47 || e.charCode === 47 || e.key === "AKEYCODE_SLASH" || e.key === "VK_DIVIDE") {
+				// TODO: Update this to somehow toggle through search bars if focusing fails
 				slearch.bars[0].focus();
 				slearch.bars[0].scrollIntoView();
+				// TODO: remove the slash which gets added into input value
 			}
 		};
+	},
+
+	init: function() {
+		slearch.getHtmlSearchBars();
+		slearch.getPseudoSearchBars();
+		slearch.mapActions();
 	}
 
 };
 
-// Find all search bars
-slearch.getHtmlSearchBars();
-slearch.getPseudoSearchBars();
-slearch.mapActions();
-
-// TODO: map the "/" so that it toggles through all the search bars found on the page
-
-// For debug. TOFO: delete
-if (slearch.bars.length > 0) {
-	console.log("Slearch says: there are "+ slearch.bars.length +" search bars on this page:", slearch.bars);
-} else {
-	console.log("no search bars found");
-}
+// Initialize
+slearch.init();
